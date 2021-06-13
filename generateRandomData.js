36052ba87096079413417619bd09e146feea8037
Md. Ehtisham Farooqui <mefxe01@gmail.com>
2021-06-13 23:48:04 +0530

    added new functions

---
+++ generateRandomData.js
@@ -0,0 +1,20 @@
+// ASCII for random data
+// 48-57 for numbers 0 to 9.
+// 65-90 for Uppercase A to Z.
+// 97-122 for Lowercase a to z.
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97) // this will generate random Lowercase letters from a to z
+}
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65) // this will generate random Uppercase letters from A to Z
+}
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48) // this will generate random numbers
+}
+
+// I am going to use a perticular set of symbols
+function getRandomSymbol() {
+  let symbols = "!@#$%^&*()_+-[]{}/.,<>"
+  return symbols[Math.floor(Math.random() * symbols.length)]
+}
